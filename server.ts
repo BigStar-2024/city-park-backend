8eacd34354eb652c4bb5377511f4436c136658b0
BigStar2024 <aadsf7463@gmail.com>
2024-05-23 21:31:42 +0000

    add socket

--- server.ts
+++ server.ts
@@ -4,6 +4,9 @@ import morgan from 'morgan'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import path from 'path'
+import http from 'http'
+import { Server } from 'socket.io';
+
 import connectToMongodb from './db/mongodb';
 import { authEndUserMiddleware, authSenderMiddleware } from './middleware/auth';
 import endUserRouter from './routers/end';
@@ -23,6 +26,22 @@ mainRouter.use('/end-user', authEndUserMiddleware, endUserRouter)
 mainRouter.use('/sender', authSenderMiddleware, senderRouter)
 app.use(process.env.NODE_ENV === "production" ? '/city-park-lot/api' : '/', mainRouter)
 
-app.listen(port, () => {
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('send_message', (message: string) => {
+        io.emit('receive_message', message);
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
