c4691209f4f0e99b257f9231932f4838699f5c69
BigStar2024 <aadsf7463@gmail.com>
2024-05-31 06:16:28 +0000

    feat: add payingapp/data

--- routers/payingapp/index.ts
+++ routers/payingapp/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import lotModel from "../../models/lot";
+import dataModel from "../../models/data";
 
 const payingAppLouter = express.Router();
 
@@ -7,7 +8,23 @@ payingAppLouter
     .route("/lot")
     .get(async (req, res) => {
         const lot = await lotModel.find();
-        res.status(200).json(lot);
+
+        if(lot) {
+            res.status(200).json(lot);
+        } else {
+            res.status(500).json({message: "server error"});
+        }
+    })
+
+payingAppLouter
+    .route("/data")
+    .get(async (req, res) => {
+        let dataList = await dataModel.find().sort({ time: -1 });
+        if(dataList) {
+            res.status(200).json(dataList)
+        } else {
+            res.status(500).json({message: "No data"});
+        }
     })
 
 export default payingAppLouter;
