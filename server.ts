2eaf158bd3e5ea0780b81da5cdd8adb4ac8eb81d
BigStar2024 <aadsf7463@gmail.com>
2024-06-06 04:49:30 +0000

    update

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Application } from 'express';
+import express, { Application } from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan'
 import cors from 'cors'
@@ -13,12 +13,13 @@ import { authEndUserMiddleware, authSenderMiddleware } from './middleware/auth';
 import endUserRouter from './routers/end';
 import senderRouter from './routers/sender';
 import payingAppRouter from './routers/payingapp';
-import paymentLog from './models/paymentLog'
-const mainRouter = express.Router();
+import paymentLog from './models/paymentLog';
 dotenv.config();
 connectToMongodb()
+
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(cors());
@@ -30,7 +31,9 @@ app.use(cors());
   }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 endUserRouter.use('/public', express.static(path.join(__dirname, process.env.NODE_ENV === "production" ? '../public' : 'public')))
+const mainRouter = express.Router();
 mainRouter.use('/end-user', authEndUserMiddleware, endUserRouter)
 mainRouter.use('/sender', authSenderMiddleware, senderRouter)
 mainRouter.use('/payingapp', payingAppRouter)
@@ -48,6 +51,7 @@ app.get('/city-park-lot/api/end-user/getPassDataCount', async (req, res) => {
         res.json(documentCount);        
     } catch (error: any) {
         console.log(`Error connecting to DB: ${(error).message}`);
+        res.status(500).send('Internal Server Error');
         process.exit(1);
     }
 });
@@ -60,6 +64,7 @@ app.get('/city-park-lot/api/end-user/getPaidData', async (req, res) => {
         res.json(paidData);        
     } catch (error: any) {
         console.log(`Error connecting to DB: ${(error).message}`);
+        res.status(500).send('Internal Server Error');
         process.exit(1);
     }
 });
@@ -90,3 +95,6 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+export{ io };
